a27af7f0452440bae25ab38cdf2f236ac963bfba
nathanmcconnell42 <nathanmcconnell7@gmail.com>
2018-01-27 14:22:33 -0800

    added pid disable and interrupt

--- 2018 Robot/src/org/usfirst/frc/team4999/commands/autonomous/ForwardBackward.java
+++ 2018 Robot/src/org/usfirst/frc/team4999/commands/autonomous/ForwardBackward.java
@@ -116,10 +116,12 @@ public class ForwardBackward extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	pid.disable();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	pid.disable();
     }
 }
