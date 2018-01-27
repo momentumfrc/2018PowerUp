24df5d0687877f720cd4047db12c1b405551d764
nathanmcconnell42 <nathanmcconnell7@gmail.com>
2018-01-26 22:21:28 -0800

    Added 2 new packages for command groups(alliance specific)
    also 12 new classes, thats right 12! im breaking down everything to its
    simplest form, so it can be easily be out on the smart dashboard.
    UNBELIVABLY TEDIOUS

---
+++ 2018 Robot/src/org/usfirst/frc/team4999/robot/commandgroups/bluealliance/PlaceCubeOnSwitchLeft.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc.team4999.robot.commandgroups.bluealliance;
+
+import org.usfirst.frc.team4999.commands.autonomous.ForwardBackward;
+import org.usfirst.frc.team4999.commands.autonomous.TurnByDeg;
+import org.usfirst.frc.team4999.robot.commands.lift.MoveLift;
+import org.usfirst.frc.team4999.robot.commands.lift.SetLiftToZero;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class PlaceCubeOnSwitchLeft extends CommandGroup {
+
+    public PlaceCubeOnSwitchLeft() {
+    	addSequential(new ForwardBackward(5));// put random dist, will tune at future meetings
+    	addSequential(new TurnByDeg(90));
+    	addSequential(new MoveLift(10)); // tune that dist obvi
+    	//need to write command to 'shoot' out cube
+    	addSequential(new SetLiftToZero());
+    	addSequential(new ForwardBackward(-5)); // get outta there (again, tune the dist)
+    }
+}
