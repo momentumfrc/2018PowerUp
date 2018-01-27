1eb11a46737dcb78b3f8bbd429e283ffdc36c17b
nathanmcconnell42 <nathanmcconnell7@gmail.com>
2018-01-26 21:53:17 -0800

    Finished the turning (wow that was easier than distance)
    added the actual turn code, as well as various init, end, etc

--- 2018 Robot/src/org/usfirst/frc/team4999/commands/autonomous/TurnByDeg.java
+++ 2018 Robot/src/org/usfirst/frc/team4999/commands/autonomous/TurnByDeg.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team4999.commands.autonomous;
 
+import org.usfirst.frc.team4999.robot.Robot;
 import org.usfirst.frc.team4999.robot.RobotMap;
 import org.usfirst.frc.team4999.robot.subsystems.DriveSystem;
 
@@ -86,13 +87,20 @@ public class TurnByDeg extends Command {
 		
 	}
 	
-    public TurnByDeg() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+    public TurnByDeg(double angle) {
+    	requires(Robot.driveSystem);
+    	this.angle = angle;
+    	angleGetter = new EncoderTurnOutput(left, right);
+    	angleController = new PIDController(0.2,0,0.01,new EncoderTurnOutput(left, right),new DriveTurn(Robot.driveSystem)
+    			);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	left.setDistancePerPulse(1/ticksPerMeter);
+    	right.setDistancePerPulse(1/ticksPerMeter);
+    	angleController.setSetpoint(angleGetter.getAngle() + angle);
+    	angleController.enable();
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -101,16 +109,18 @@ public class TurnByDeg extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return angleController.onTarget();
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	angleController.disable();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	angleController.disable();
     }
 
 }
