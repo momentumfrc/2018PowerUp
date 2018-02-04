e94ac0f642247016d3d15ac5525443c2a01375e0
nathanmcconnell42 <nathanmcconnell7@gmail.com>
2018-02-04 15:24:48 -0800

    Added the RotationSource class

---
+++ PythonAuto/RotationSource.py
@@ -0,0 +1,13 @@
+import sys
+import imp
+
+sys.path.append('/usr/local/lib/vmxpi/')
+vmxpi = imp.load_source('vmxpi_hai_python', '/usr/local/lib/vmxpi/vmxpi_hai_python.py')
+
+class RotationSource:
+
+    def __init__(self, vmx):
+        self.vmx = vmx
+
+    def get(self): #return the YAW value of the gyro for use in the PidController
+        return vmx.getAHRS().getYaw()
