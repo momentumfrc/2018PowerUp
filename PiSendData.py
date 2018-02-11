eeac2ea7191423b24fe8f5c3ba0da8a57a911b79
Jordan Powers <jordanpowers1227@gmail.com>
2018-02-11 12:36:34 -0800

    Detect loss of connection

--- PiSendData.py
+++ PiSendData.py
@@ -22,9 +22,18 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 while True:
     angle = vmx.getAHRS().GetAngle()
     rate = vmx.getAHRS().GetRate()
-    struct.pack_into(">d", out, 0, angle)
-    struct.pack_into(">d", out, 8, rate)
-
-    sock.sendto(out, (SERVER_IP, SERVER_PORT))
+    xV = vmx.getAHRS().GetVelocityX()
+    yV = vmx.getAHRS().GetVelocityY()
+    struct.pack_into(">d", out, 1, angle)
+    struct.pack_into(">d", out, 9, rate)
+    chksum = sum(out[1:]) % 255
+    struct.pack_into(">B", out, 0, chksum)
+    if vmx.getAHRS().IsCalibrating():
+        print("Calibrating... angle: %.2f, rate: %.2f, xV: %.2f, yV:%.2f" % (angle, rate, xV, yV))
+    elif not vmx.getAHRS().IsConnected():
+        print("Not connected...")
+    else:
+        print("Sending angle %.2f rate %.2f, xV: %.2f, yV:%.2f" % (angle, rate, xV, yV))
+        sock.sendto(out, (SERVER_IP, SERVER_PORT))
 
     time.DelayMilliseconds(DELAY)
