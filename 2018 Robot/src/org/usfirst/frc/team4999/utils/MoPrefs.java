165fd1d9e68c9a6198e7c376b33e8add5f576233
Jordan Powers <jordanpowers1227@gmail.com>
2018-08-25 19:55:30 -0700

    Cleaned up MoPrefs

--- 2018 Robot/src/org/usfirst/frc/team4999/utils/MoPrefs.java
+++ 2018 Robot/src/org/usfirst/frc/team4999/utils/MoPrefs.java
@@ -11,53 +11,79 @@ public class MoPrefs {
 	private static final double LIFT_ENC_TICKS = 500; 			// Number of lift encoder ticks per meter of travel
 	private static final double FALLBACK_AUTO_TIME = 3; 		// Time, in seconds, the robot will drive for when in fallback auto mode
 	private static final double FALLBACK_AUTO_DISTANCE = 3.5; 	// Distance the robot will drive for, in meters, in fallback auto mode
+	private static final double SWITCH_ELBOW_ANGLE = 20; 		// Angle the elbow is set to when placing a cube on the switch in autonomous
 	private static final double MAX_LIFT_HEIGHT = 2; 			// Maximum height of the lift in meters
 	private static final int 	MAX_ELBOW_ROTATION = 700;		// Maximum encoder ticks the elbow can displace
-	private static final double ELBOW_ENC_TICKS = 0.0194;		// Encoder ticks per degree
+	private static final double ELBOW_ENC_TICKS = 0.0194;		// Encoder ticks per degree of elbow rotation
 	
 	
-	private static void checkDouble(String key, double def) {
+	private static double getDouble(String key, double def) {
 		if(!prefs.containsKey(key)) {
 			prefs.putDouble(key, def);
 		}
+		return prefs.getDouble(key,  def);
 	}
 	
-	private static void checkInt(String key, int def) {
+	private static int getInt(String key, int def) {
 		if(!prefs.containsKey(key)) {
 			prefs.putInt(key, def);
 		}
+		return prefs.getInt(key, def);
 	}
+	
+	/**
+	 * Gets the maximum speed the robot will travel at in autonomous
+	 */
 	public static double getAutoSpeed() {
-		checkDouble("AUTO_SPEED", AUTO_SPEED);
-		return prefs.getDouble("AUTO_SPEED", AUTO_SPEED);
+		return getDouble("AUTO_SPEED", AUTO_SPEED);
 	}
+	/**
+	 * Gets the number of encoder ticks per meter of travel
+	 */
 	public static double getDriveEncTicks() {
-		checkDouble("DRIVE_ENC_TICKS", DRIVE_ENC_TICKS);
-		return prefs.getDouble("DRIVE_ENC_TICKS", DRIVE_ENC_TICKS);
+		return getDouble("DRIVE_ENC_TICKS", DRIVE_ENC_TICKS);
 	}
-	public static double getFallbackAutoTime() {
-		checkDouble("FALLBACK_AUTO_TIME",FALLBACK_AUTO_TIME);
-		return prefs.getDouble("FALLBACK_AUTO_TIME", FALLBACK_AUTO_TIME);
-	}
-	public static double getFallbackAutoDistance() {
-		checkDouble("FALLBACK_AUTO_DISTANCE", FALLBACK_AUTO_DISTANCE);
-		return prefs.getDouble("FALLBACK_AUTO_DISTANCE", FALLBACK_AUTO_DISTANCE);
-	}
+	/**
+	 * Gets the number of lift encoder ticks per meter of travel
+	 */
 	public static double getLiftEncTicks() {
-		checkDouble("LIFT_ENC_TICKS",LIFT_ENC_TICKS);
-		return prefs.getDouble("LIFT_ENC_TICKS", LIFT_ENC_TICKS);
+		return getDouble("LIFT_ENC_TICKS",LIFT_ENC_TICKS);
 	}
+	/**
+	 * Gets the time, in seconds, the robot will drive for when in fallback auto mode
+	 */
+	public static double getFallbackAutoTime() {
+		return getDouble("FALLBACK_AUTO_TIME",FALLBACK_AUTO_TIME);
+	}
+	/**
+	 * Gets the distance the robot will drive for, in meters, in fallback auto mode
+	 */
+	public static double getFallbackAutoDistance() {
+		return getDouble("FALLBACK_AUTO_DISTANCE", FALLBACK_AUTO_DISTANCE);
+	}
+	/**
+	 * Gets the angle the elbow is set to when placing a cube on the switch in autonomous
+	 */
+	public static double getSwitchElbowAngle() {
+		return getDouble("SWITCH_ELBOW_ANGLE", SWITCH_ELBOW_ANGLE);
+	}
+	/**
+	 * Gets the maximum height of the lift in meters
+	 */
 	public static double getMaxLiftHeight() {
-		checkDouble("MAX_LIFT_HEIGHT", MAX_LIFT_HEIGHT);
-		return prefs.getDouble("MAX_LIFT_HEIGHT", MAX_LIFT_HEIGHT);
+		return getDouble("MAX_LIFT_HEIGHT", MAX_LIFT_HEIGHT);
 	}
+	/**
+	 * Gets the maximum encoder ticks the elbow can displace
+	 */
 	public static int getMaxElbowRotation() {
-		checkInt("MAX_ELBOW_ROTATION", MAX_ELBOW_ROTATION);
-		return prefs.getInt("MAX_ELBOW_ROTATION", MAX_ELBOW_ROTATION);
+		return getInt("MAX_ELBOW_ROTATION", MAX_ELBOW_ROTATION);
 	}
+	/**
+	 * Gets the encoder ticks per degree of elbow rotation
+	 */
 	public static double getElbowEncTicks() {
-		checkDouble("ELBOW_ENC_TICKS", ELBOW_ENC_TICKS);
-		return prefs.getDouble("ELBOW_ENC_TICKS", ELBOW_ENC_TICKS);
+		return getDouble("ELBOW_ENC_TICKS", ELBOW_ENC_TICKS);
 	}
 	
 
