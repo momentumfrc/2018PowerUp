cd2c5a49aa5081c44dad60acc0437519205fe717
JCook <jacob_a_cook@outlook.com>
2018-08-30 22:21:32 -0700

    Scale Autos

--- 2018 Robot/src/org/usfirst/frc/team4999/commands/autonomous/paths/PlaceCubeOnScale.java
+++ 2018 Robot/src/org/usfirst/frc/team4999/commands/autonomous/paths/PlaceCubeOnScale.java
@@ -2,6 +2,9 @@ package org.usfirst.frc.team4999.commands.autonomous.paths;
 
 import org.usfirst.frc.team4999.commands.autonomous.MoveDistance;
 import org.usfirst.frc.team4999.commands.autonomous.TurnDegrees;
+import org.usfirst.frc.team4999.commands.elbow.SetElbowPosition;
+import org.usfirst.frc.team4999.commands.intake.Shoot;
+import org.usfirst.frc.team4999.commands.lift.SetLiftHeight;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -18,8 +21,12 @@ public class PlaceCubeOnScale extends CommandGroup {
         		addSequential(new MoveDistance(7.61111));
         		break;
         	case RIGHT:
-        		addSequential(new MoveDistance(1.5));
+        		addSequential(new MoveDistance(1));
         		addSequential(new TurnDegrees(90));
+        		addSequential(new MoveDistance(6));
+        		addSequential(new TurnDegrees(-90));
+        		addSequential(new MoveDistance(6.61111));
+        		
         		break;
         	}
         	break;
@@ -37,9 +44,17 @@ public class PlaceCubeOnScale extends CommandGroup {
         		addSequential(new MoveDistance(7.61111));
         		break;
         	case RIGHT:
+        		addSequential(new MoveDistance(1));
+        		addSequential(new TurnDegrees(-90));
+        		addSequential(new MoveDistance(6));
+        		addSequential(new TurnDegrees(90));
+        		addSequential(new MoveDistance(6.61111));
         		break;
         	}
         	break;
         }
+		addSequential(new SetLiftHeight(.5, true));//TODO Figure out the lift height for Max scale 
+		addSequential(new SetElbowPosition(90));
+		addSequential(new Shoot());
     }
 }
