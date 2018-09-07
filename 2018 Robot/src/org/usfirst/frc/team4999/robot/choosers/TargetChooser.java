9d34dc2362cda4cd98f8467b5c5a884d6da89047
JCook <jacob_a_cook@outlook.com>
2018-09-06 22:34:35 -0700

    Added a Target Chooser
    For Off season events

---
+++ 2018 Robot/src/org/usfirst/frc/team4999/robot/choosers/TargetChooser.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team4999.robot.choosers;
+
+import org.usfirst.frc.team4999.commands.autonomous.paths.TargetPosition;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class TargetChooser extends SendableChooser<TargetPosition>{
+	public static final String NAME = "Target Chooser";
+
+	public TargetChooser() {
+		super();
+		addDefault("Left", TargetPosition.LEFT);
+		addObject("Right", TargetPosition.RIGHT);
+		SmartDashboard.putData(NAME, this);
+		}
+}
